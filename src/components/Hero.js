f09a6bf8bf2d8c955a32af000c66551b6ec2b4c3
Raj <rajshetkar12@gmail.com>
2025-03-05 17:34:32 +0530

    2nd new landing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,9 +5,10 @@ const Hero = () => {
     return (
         <div className="hero-container">
             <div className="hero-content">
-                <h1 className="hero-heading">Transforming Spaces with Timeless Clay Art</h1>
-                <p className="hero-subtitle">Handcrafted by Amar</p>
-                <button className="hero-button">Explore Artworks</button>
+                <h1 className="hero-heading">Amar Shet's</h1>
+                <div className="hero-divider"></div> {/* Divider */}
+                <p className="hero-subtitle">Mayem House of Pottry</p>
+                {/*<button className="hero-button">Explore Artworks</button>*/}
             </div>
         </div>
     );
